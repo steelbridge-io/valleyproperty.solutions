0bed2806829435323f3a8f2837380c7ba4411e47
Chris Parsons <chrisparsons@Chris-MacBook-Pro.local>
2019-09-10 19:05:46 -0700

    first commit

---
+++ public/component---src-pages-404-js-d482940f2b7288a9316a.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{204:function(e,t,a){"use strict";a.r(t);var n=a(0),l=a.n(n),s=a(201);t.default=function(){return l.a.createElement(s.a,null,l.a.createElement("div",{id:"main",className:"alt"},l.a.createElement("section",{id:"one"},l.a.createElement("div",{className:"inner"},l.a.createElement("h1",null,"NOT FOUND"),l.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness.")))))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-d482940f2b7288a9316a.js.map
