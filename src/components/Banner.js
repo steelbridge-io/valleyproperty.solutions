bf0aec29d95f6d2296aa768ede296be83a318632
chris@steelbridge.io </*fa11r1VER/*/>
2019-09-26 11:31:29 -0700

    Removed list of towns in banner section

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -5,7 +5,6 @@ const Banner = (props) => (
         <div className="inner">
             <header className="major">
                 <h1>Valley Property Solutions</h1>
-                <h2>Redding<span></span>Anderson<span></span>Cottonwood<span></span>Red Bluff<span></span>Chico</h2>
             </header>
             <div className="content">
                 <p>Valley Property Solutions Inc. is a renowned environmental & engineering consulting firm based in Northern California, specializing in commercial real estate transactions and construction management.</p>
