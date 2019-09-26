a66395089a2f3cc16d9aa19bb6d5e431091b3645
chris@steelbridge.io </*fa11r1VER/*/>
2019-09-26 11:19:37 -0700

    Updated scss >> banner :after color

--- .cache/api-runner-browser-plugins.js
+++ .cache/api-runner-browser-plugins.js
@@ -1,7 +1,7 @@
 module.exports = [{
-      plugin: require('/Users/chrisparsons/git/vps-forty/valleyproperty.solutions/node_modules/gatsby-plugin-offline/gatsby-browser.js'),
+      plugin: require('/Users/chrisparsons/git/valleyproperty.solutions/node_modules/gatsby-plugin-offline/gatsby-browser.js'),
       options: {"plugins":[]},
     },{
-      plugin: require('/Users/chrisparsons/git/vps-forty/valleyproperty.solutions/gatsby-browser.js'),
+      plugin: require('/Users/chrisparsons/git/valleyproperty.solutions/gatsby-browser.js'),
       options: {"plugins":[]},
     }]
--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,11 +5,11 @@ const preferDefault = m => m && m.default || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/chrisparsons/git/vps-forty/valleyproperty.solutions/.cache/dev-404-page.js"))),
-  "component---src-pages-404-js": hot(preferDefault(require("/Users/chrisparsons/git/vps-forty/valleyproperty.solutions/src/pages/404.js"))),
-  "component---src-pages-elements-js": hot(preferDefault(require("/Users/chrisparsons/git/vps-forty/valleyproperty.solutions/src/pages/elements.js"))),
-  "component---src-pages-generic-js": hot(preferDefault(require("/Users/chrisparsons/git/vps-forty/valleyproperty.solutions/src/pages/generic.js"))),
-  "component---src-pages-index-js": hot(preferDefault(require("/Users/chrisparsons/git/vps-forty/valleyproperty.solutions/src/pages/index.js"))),
-  "component---src-pages-landing-js": hot(preferDefault(require("/Users/chrisparsons/git/vps-forty/valleyproperty.solutions/src/pages/landing.js")))
+  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/chrisparsons/git/valleyproperty.solutions/.cache/dev-404-page.js"))),
+  "component---src-pages-404-js": hot(preferDefault(require("/Users/chrisparsons/git/valleyproperty.solutions/src/pages/404.js"))),
+  "component---src-pages-elements-js": hot(preferDefault(require("/Users/chrisparsons/git/valleyproperty.solutions/src/pages/elements.js"))),
+  "component---src-pages-generic-js": hot(preferDefault(require("/Users/chrisparsons/git/valleyproperty.solutions/src/pages/generic.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("/Users/chrisparsons/git/valleyproperty.solutions/src/pages/index.js"))),
+  "component---src-pages-landing-js": hot(preferDefault(require("/Users/chrisparsons/git/valleyproperty.solutions/src/pages/landing.js")))
 }
 
