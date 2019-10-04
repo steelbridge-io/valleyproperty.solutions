784ddd7f90a55487ed9ce0af171b9a7c35ed7441
steelbridge-io <chris@steelbridge.io>
2019-10-03 20:24:43 -0700

    Added social media links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,9 +11,9 @@ const Footer = (props) => {
             <div className="inner">
                 <ul className="icons">
                     <li><a href="https://twitter.com/valley_property" className="icon alt fa-twitter"><span className="label">Twitter</span></a></li>
-                    <li><a href="#" className="icon alt fa-facebook"><span className="label">Facebook</span></a></li>
+                    <li><a href="https://www.facebook.com/valleyproperty.solutions13/" className="icon alt fa-facebook"><span className="label">Facebook</span></a></li>
                     <li><a href="https://www.instagram.com/valleyproperty.solutions/" className="icon alt fa-instagram"><span className="label">Instagram</span></a></li>
-                    <li><a href="#" className="icon alt fa-linkedin"><span className="label">LinkedIn</span></a></li>
+                    <li><a href="https://www.linkedin.com/in/christian-magana-232441194/" className="icon alt fa-linkedin"><span className="label">LinkedIn</span></a></li>
                 </ul>
 
                 <ul className="copyright">
