63d067f78e468c332772f616a4af19e915311f29
steelbridge-io <chris@steelbridge.io>
2020-01-19 21:37:39 -0800

    Updated Properties We Purchase page.

--- .cache/async-requires.js
+++ .cache/async-requires.js
@@ -2,20 +2,20 @@
 const preferDefault = m => m && m.default || m
 
 exports.components = {
-  "component---cache-dev-404-page-js": () => import("/Users/chrisparsons/git/valleyproperty.solutions/.cache/dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
-  "component---src-pages-404-js": () => import("/Users/chrisparsons/git/valleyproperty.solutions/src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */),
-  "component---src-pages-about-valley-property-solutions-js": () => import("/Users/chrisparsons/git/valleyproperty.solutions/src/pages/about-valley-property-solutions.js" /* webpackChunkName: "component---src-pages-about-valley-property-solutions-js" */),
-  "component---src-pages-common-reasons-for-selling-js": () => import("/Users/chrisparsons/git/valleyproperty.solutions/src/pages/common-reasons-for-selling.js" /* webpackChunkName: "component---src-pages-common-reasons-for-selling-js" */),
-  "component---src-pages-contact-form-thank-you-js": () => import("/Users/chrisparsons/git/valleyproperty.solutions/src/pages/contact-form-thank-you.js" /* webpackChunkName: "component---src-pages-contact-form-thank-you-js" */),
-  "component---src-pages-contact-us-js": () => import("/Users/chrisparsons/git/valleyproperty.solutions/src/pages/contact-us.js" /* webpackChunkName: "component---src-pages-contact-us-js" */),
-  "component---src-pages-elements-js": () => import("/Users/chrisparsons/git/valleyproperty.solutions/src/pages/elements.js" /* webpackChunkName: "component---src-pages-elements-js" */),
-  "component---src-pages-generic-js": () => import("/Users/chrisparsons/git/valleyproperty.solutions/src/pages/generic.js" /* webpackChunkName: "component---src-pages-generic-js" */),
-  "component---src-pages-index-js": () => import("/Users/chrisparsons/git/valleyproperty.solutions/src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */),
-  "component---src-pages-join-valley-property-solutions-js": () => import("/Users/chrisparsons/git/valleyproperty.solutions/src/pages/join-valley-property-solutions.js" /* webpackChunkName: "component---src-pages-join-valley-property-solutions-js" */),
-  "component---src-pages-landing-js": () => import("/Users/chrisparsons/git/valleyproperty.solutions/src/pages/landing.js" /* webpackChunkName: "component---src-pages-landing-js" */),
-  "component---src-pages-projects-js": () => import("/Users/chrisparsons/git/valleyproperty.solutions/src/pages/projects.js" /* webpackChunkName: "component---src-pages-projects-js" */),
-  "component---src-pages-properties-we-purchase-js": () => import("/Users/chrisparsons/git/valleyproperty.solutions/src/pages/properties-we-purchase.js" /* webpackChunkName: "component---src-pages-properties-we-purchase-js" */),
-  "component---src-pages-wholesailing-js": () => import("/Users/chrisparsons/git/valleyproperty.solutions/src/pages/wholesailing.js" /* webpackChunkName: "component---src-pages-wholesailing-js" */),
-  "component---src-pages-wholesale-home-help-services-js": () => import("/Users/chrisparsons/git/valleyproperty.solutions/src/pages/wholesale-home-help-services.js" /* webpackChunkName: "component---src-pages-wholesale-home-help-services-js" */)
+  "component---cache-dev-404-page-js": () => import("./dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
+  "component---src-pages-404-js": () => import("./../src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */),
+  "component---src-pages-about-valley-property-solutions-js": () => import("./../src/pages/about-valley-property-solutions.js" /* webpackChunkName: "component---src-pages-about-valley-property-solutions-js" */),
+  "component---src-pages-common-reasons-for-selling-js": () => import("./../src/pages/common-reasons-for-selling.js" /* webpackChunkName: "component---src-pages-common-reasons-for-selling-js" */),
+  "component---src-pages-contact-form-thank-you-js": () => import("./../src/pages/contact-form-thank-you.js" /* webpackChunkName: "component---src-pages-contact-form-thank-you-js" */),
+  "component---src-pages-contact-us-js": () => import("./../src/pages/contact-us.js" /* webpackChunkName: "component---src-pages-contact-us-js" */),
+  "component---src-pages-elements-js": () => import("./../src/pages/elements.js" /* webpackChunkName: "component---src-pages-elements-js" */),
+  "component---src-pages-generic-js": () => import("./../src/pages/generic.js" /* webpackChunkName: "component---src-pages-generic-js" */),
+  "component---src-pages-index-js": () => import("./../src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */),
+  "component---src-pages-join-valley-property-solutions-js": () => import("./../src/pages/join-valley-property-solutions.js" /* webpackChunkName: "component---src-pages-join-valley-property-solutions-js" */),
+  "component---src-pages-landing-js": () => import("./../src/pages/landing.js" /* webpackChunkName: "component---src-pages-landing-js" */),
+  "component---src-pages-projects-js": () => import("./../src/pages/projects.js" /* webpackChunkName: "component---src-pages-projects-js" */),
+  "component---src-pages-properties-we-purchase-js": () => import("./../src/pages/properties-we-purchase.js" /* webpackChunkName: "component---src-pages-properties-we-purchase-js" */),
+  "component---src-pages-wholesailing-js": () => import("./../src/pages/wholesailing.js" /* webpackChunkName: "component---src-pages-wholesailing-js" */),
+  "component---src-pages-wholesale-home-help-services-js": () => import("./../src/pages/wholesale-home-help-services.js" /* webpackChunkName: "component---src-pages-wholesale-home-help-services-js" */)
 }
 
